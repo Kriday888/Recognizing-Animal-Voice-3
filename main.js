0c0576275f7f900f73875b4c815ff780a1a5ff11
Kriday888 <91681834+Kriday888@users.noreply.github.com>
2022-08-05 20:06:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/xLGDp4kko/model.json', modelReady);
+}
+function modelReady(){
+    classifier.classify(gotResults);
+}
+function gotResults(error, results){
+    if(error){
+    console.error(error);
+    } else{
+        console.log(results);
+        random_number_r=Math.floor(Math.random()* 255)+ 1;
+        random_number_g=Math.floor(Math.random()* 255)+ 1;
+        random_number_b=Math.floor(Math.random()* 255)+ 1;
+        document.getElementById("result_label").innerHTML='I can Hear - '+ results[0].label;
+        document.getElementById("result_confidence").innerHTML='Accuracy - '+( results[0].confidence*100).toFixed(2)+" %";
+        document.getElementById("result_label").style.color="rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+        document.getElementById("result_confidence").style.color="rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+        img=document.getElementById('alien1');
+        img1=document.getElementById('alien2');
+        img2=document.getElementById('alien3');
+        img3=document.getElementById('alien4');
+        if(results[0].label=="Mooing"){
+            img.src='Cow.gif';
+            img1.src='Dog.jpg';
+            img2.src='Cat.jpg';
+            img3.src='alien.jpg';
+        }
+        else if(results[0].label=="Barking"){
+            img.src='Cow.jpg';
+            img1.src='Dog.gif';
+            img2.src='Cat.jpg';
+            img3.src='alien.jpg';
+        }
+        else if(results[0].label=="Meowing"){
+            img.src='Cow.jpg';
+            img1.src='Dog.jpg';
+            img2.src='Cat.gif';
+            img3.src='alien.jpg';
+        }
+        else{
+            img.src='Cow.jpg';
+            img1.src='Dog.jpg';
+            img2.src='Cat.jpg';
+            img3.src='alien.gif';
+        }
+    }
+    
+}
